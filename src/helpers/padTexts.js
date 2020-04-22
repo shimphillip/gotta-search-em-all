bd61cc4ec7ceca02129260dcaaf07516caa7ee77
shimphillip <shimphillip@gmail.com>
2020-04-21 23:40:12 -0500

    convert helper functions to ts

--- src/helpers/padTexts.js
+++
@@ -1,8 +0,0 @@
-export const padTexts = (name, value, symbol, length) => {
-  value = value || '0';
-  const output = `
-      ${name.toString()}${symbol.repeat(
-    length - (value.toString().length + name.toString().length)
-  )}${value.toString()}`;
-  return output;
-};
--- src/helpers/pickRandom.js
+++
@@ -1,1 +0,0 @@
-export const pickRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
