5ff569ad3b9e191b72336734aa8d4fef5c710bb3
shimphillip <shimphillip@gmail.com>
2020-05-02 12:26:59 -0500

    dynamically change background picture

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -5,6 +5,7 @@ import Divider from './Divider'
 import RightPanel from './RightPanel'
 import { usePokemon } from '../hooks'
 import { Loading } from './shared'
+import { Global, css } from '@emotion/core'
 
 const Pokedex = () => {
   const { pokemon, pokemonIndex, loading, changePokemonIndex } = usePokemon()
@@ -13,8 +14,32 @@ const Pokedex = () => {
     return <Loading noBackground />
   }
 
+  const getBackground = (): string => {
+    // pokemonIndex is guaranteed to be above 0
+    if (pokemonIndex < 152 && pokemon?.pokemonData.name) {
+      console.log(pokemon?.pokemonData.name)
+
+      if (pokemon?.pokemonData.name === 'nidoran-f') {
+        return 'nidoran-female'
+      }
+      if (pokemon?.pokemonData.name === 'nidoran-m') {
+        return 'nidoran-male'
+      }
+
+      return pokemon?.pokemonData.name
+    }
+    return 'pikachu'
+  }
+
   return (
     <Container>
+      <Global
+        styles={css`
+          body {
+            background-image: url(/backgrounds/${getBackground()}.png);
+          }
+        `}
+      />
       <div className="inner-container">
         <LeftPanel pokemon={pokemon} pokemonIndex={pokemonIndex} />
         <Divider />
