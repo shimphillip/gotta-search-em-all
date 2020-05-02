369c273630ab3b4b8cb8a6795e44e583ef2c5843
shimphillip <shimphillip@gmail.com>
2020-05-02 13:46:44 -0500

    add opaicty to the pokedex

--- src/components/PokedexStyles.ts
+++ src/components/PokedexStyles.ts
@@ -5,6 +5,7 @@ const Container = styled.div`
   background: ${({ theme }) => theme.colors.pokedexColor};
   padding: 5px 0;
   border-radius: 50px;
+  opacity: 0.90;
 
   .inner-container {
     position: relative;
